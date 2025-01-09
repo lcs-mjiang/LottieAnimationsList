3565af54de6620637a514271c0769f989f2282e3
Michael Jiang <mjiang@lcs.on.ca>
2025-01-09 14:46:09 -0500

    Initial Commit

---
+++ LottieAnimationsList/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LottieAnimationsList
+//
+//  Created by Michael on 2025/1/9.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by Michael on 2025/1/9.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
